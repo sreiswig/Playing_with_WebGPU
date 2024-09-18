1553fd553dbdf21af2f544815fc8409afa627cc0
sreiswig <sam.reiswig@gmail.com>
2024-09-18 05:52:06 -0500

    should be a black canvas

---
+++ hello_web_gpu.js
@@ -0,0 +1,29 @@
+const adapter = await navigator.gpu.requestAdapter();
+
+if (!navigator.gpu) {
+  throw new Error("WebGPU not supported on this browser.");
+}
+
+const device = await adapter.requestDevice();
+const context = canvas.getContext("webgpu");
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+context.configure({
+  device: device,
+  format: canvasFormat,
+});
+
+const encoder = device.createCommandEncoder();
+const pass = encoder.beginRenderPass({
+  colorAttachments: [{
+     view: context.getCurrentTexture().createView(),
+     loadOp: "clear",
+     storeOp: "store",
+  ]}
+});
+
+pass.end();
+
+const commandBuffer = encoder.finish();
+device.queue.submit([commandBuffer]);
+device.queue.submit([encoder.finish()]);
+
