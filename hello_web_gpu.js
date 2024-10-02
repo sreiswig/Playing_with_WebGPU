3ca77c9055167949fc4c67b65f69b1c16b66c309
sreiswig <sam.reiswig@gmail.com>
2024-10-02 06:28:06 -0500

    first buffer

--- hello_web_gpu.js
+++ hello_web_gpu.js
@@ -12,6 +12,65 @@ context.configure({
   device: device,
   format: canvasFormat,
 });
+const vertices = new Float32Array([
+  //   X,    Y,
+    -0.8, -0.8,
+     0.8, -0.8,
+     0.8,  0.8,
+
+    -0.8, -0.8,
+     0.8,  0.8,
+    -0.8,  0.8,
+]);
+
+const vertexBuffer = device.createBuffer({
+  label: "Cell vertices",
+  size: vertices.byteLength,
+  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+
+device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+
+const vertexBufferLayout = {
+  arrayStride: 8,
+  attributes: [{
+    format: "float32x2",
+    offset: 0,
+    shaderLocation:0,
+  }],
+};
+
+const cellShaderModule = device.createShaderModule({
+  label: "Cell shader",
+  code: `
+    @vertex
+    fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
+      return vec4f(pos, 0, 1);
+    }
+
+    @fragment
+    fn fragmentMain() -> @location(0) vec4f {
+      return vec4f(1,0,0,1);
+    }
+  `
+});
+
+const cellPipeline = device.createRenderPipeline({
+  label: "Cell pipeline",
+  layout: "auto",
+  vertex: {
+    module: cellShaderModule,
+    entryPoint: "vertexMain",
+    buffers: [vertexBufferLayout]
+  },
+  fragment: {
+    module: cellShaderModule,
+    entryPoint: "fragmentMain",
+    targets: [{
+      format: canvasFormat
+    }]
+  }
+});
 
 const encoder = device.createCommandEncoder();
 const pass = encoder.beginRenderPass({
@@ -22,6 +81,10 @@ const pass = encoder.beginRenderPass({
      storeOp: "store",
   }]
 });
+
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+pass.draw(vertices.length / 2);
 
 pass.end();
 
