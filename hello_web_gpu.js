1878e59f59461ab62eb570792efb5fce84faa214
sreiswig <sam.reiswig@gmail.com>
2024-10-07 07:50:08 -0500

    struct to javascript const

--- hello_web_gpu.js
+++ hello_web_gpu.js
@@ -50,12 +50,12 @@ const vertexBufferLayout = {
   }],
 };
 
-struct VertexInput {
+const VertexInput {
   @location(0) pos: vec2f,
   @builtin(instance_index) instance: u32,
 };
 
-struct VertexOutput {
+const VertexOutput {
   @builtin(position) pos: vec4f,
   @location(0) cell: vec2f,
 };
