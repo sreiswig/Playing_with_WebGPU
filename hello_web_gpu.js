353aaab0242cd0c563296403f29b41a5cae3825f
sreiswig <sam.reiswig@gmail.com>
2024-10-07 13:08:54 -0500

    state buffers

--- hello_web_gpu.js
+++ hello_web_gpu.js
@@ -24,18 +24,34 @@ const vertices = new Float32Array([
     -0.8,  0.8,
 ]);
 
-// Create an array representing the active state of each cell
-const cellStateArray = new Uint32Array(GRID_SIZE, GRID_SIZE);
-const cellStateStorage = device.createBuffer({
-  label: "Cell State",
-  size: cellStateArray.byteLength,
-  usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-});
+// Create an array representing the active state of each cell.
+const cellStateArray = new Uint32Array(GRID_SIZE * GRID_SIZE);
 
+// Create two storage buffers to hold the cell state.
+const cellStateStorage = [
+  device.createBuffer({
+    label: "Cell State A",
+    size: cellStateArray.byteLength,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  }),
+  device.createBuffer({
+    label: "Cell State B",
+     size: cellStateArray.byteLength,
+     usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  })
+];
+
+// Mark every third cell of the first grid as active.
 for (let i = 0; i < cellStateArray.length; i+=3) {
   cellStateArray[i] = 1;
 }
-device.queue.writeBuffer(cellStateStorage, 0, cellStateArray);
+device.queue.writeBuffer(cellStateStorage[0], 0, cellStateArray);
+
+// Mark every other cell of the second grid as active.
+for (let i = 0; i < cellStateArray.length; i++) {
+  cellStateArray[i] = i % 2;
+}
+device.queue.writeBuffer(cellStateStorage[1], 0, cellStateArray);
 
 // Create a uniform buffer that describes the grid.
 const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
@@ -122,37 +138,59 @@ const cellPipeline = device.createRenderPipeline({
   }
 });
 
-const bindGroup = device.createBindGroup({
-  label: "Cell renderer bind group",
-  layout: cellPipeline.getBindGroupLayout(0),
-  entries: [{
-    binding: 0,
-    resource: { buffer: uniformBuffer }
-  },
-  {
-    binding: 1,
-    resource: { buffer: cellStateStorage }
-  }],
-});
+const bindGroups = [
+  device.createBindGroup({
+    label: "Cell renderer bind group A",
+    layout: cellPipeline.getBindGroupLayout(0),
+    entries: [{
+      binding: 0,
+      resource: { buffer: uniformBuffer }
+    }, {
+      binding: 1,
+      resource: { buffer: cellStateStorage[0] }
+    }],
+  }),
+   device.createBindGroup({
+    label: "Cell renderer bind group B",
+    layout: cellPipeline.getBindGroupLayout(0),
+    entries: [{
+      binding: 0,
+      resource: { buffer: uniformBuffer }
+    }, {
+      binding: 1,
+      resource: { buffer: cellStateStorage[1] }
+    }],
+  })
+];
 
-const encoder = device.createCommandEncoder();
-const pass = encoder.beginRenderPass({
-  colorAttachments: [{
-     view: context.getCurrentTexture().createView(),
-     loadOp: "clear",
-     clearValue: { r: 0, g: 0, b: 0.4, a: 1},
-     storeOp: "store",
-  }]
-});
+const UPDATE_INTERVAL = 200; // Update every 200ms (5 times/sec)
+let step = 0; // Track how many simulation steps have been run
 
-pass.setPipeline(cellPipeline);
-pass.setVertexBuffer(0, vertexBuffer);
-pass.setBindGroup(0, bindGroup);
-pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE);
+// Move all of our rendering code into a function
+function updateGrid() {
+  step++; // Increment the step count
+  
+  // Start a render pass 
+  const encoder = device.createCommandEncoder();
+  const pass = encoder.beginRenderPass({
+    colorAttachments: [{
+      view: context.getCurrentTexture().createView(),
+      loadOp: "clear",
+      clearValue: { r: 0, g: 0, b: 0.4, a: 1.0 },
+      storeOp: "store",
+    }]
+  });
 
-pass.end();
+  // Draw the grid.
+  pass.setPipeline(cellPipeline);
+  pass.setBindGroup(0, bindGroups[step % 2]); // Updated!
+  pass.setVertexBuffer(0, vertexBuffer);
+  pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE);
 
-const commandBuffer = encoder.finish();
-device.queue.submit([commandBuffer]);
-device.queue.submit([encoder.finish()]);
+  // End the render pass and submit the command buffer
+  pass.end();
+  device.queue.submit([encoder.finish()]);
+}
 
+// Schedule updateGrid() to run repeatedly
+setInterval(updateGrid, UPDATE_INTERVAL);
