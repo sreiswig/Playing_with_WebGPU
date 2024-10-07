3ab57774f61743f7bf76b825df494fce2c76f07c
sreiswig <sam.reiswig@gmail.com>
2024-10-07 07:22:22 -0500

    grid

--- hello_web_gpu.js
+++ hello_web_gpu.js
@@ -56,9 +56,14 @@ const cellShaderModule = device.createShaderModule({
     @group(0) @binding(0) var<uniform> grid: vec2f;
 
     @vertex
-    fn vertexMain(@location(0) pos: vec2f) -> 
+    fn vertexMain(@location(0) pos: vec2f,
+                  @builtin(instance_index) instance: u32) -> 
       @builtin(position) vec4f {
-      return vec4f(pos / grid, 0, 1);
+      let i = f32(instance);
+      let cell = vec2f(i % grid.x, floor(i / grid.x));
+      let cellOffset = cell / grid * 2;
+      let gridPos = (pos + 1) / grid - 1 + cellOffset;
+      return vec4f(gridPos, 0, 1);
     }
 
     @fragment
