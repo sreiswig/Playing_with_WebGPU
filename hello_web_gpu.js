e116b10655f8d6a3911156d4f581f559dd1dd4d1
sreiswig <sam.reiswig@gmail.com>
2024-10-07 08:28:56 -0500

    should fix the structs

--- hello_web_gpu.js
+++ hello_web_gpu.js
@@ -53,27 +53,38 @@ const vertexBufferLayout = {
 const cellShaderModule = device.createShaderModule({
   label: "Cell shader",
   code: `
+    struct VertexInput {
+      @location(0) pos: vec2f,
+      @builtin(instance_index) instance: u32,
+    };
+    
+    struct VertexOutput {
+      @builtin(position) pos: vec4f,
+      @location(0) cell: vec2f,
+    };
+
+    struct FragInput {
+      @location(0) cell: vec2f,
+    };
+
     @group(0) @binding(0) var<uniform> grid: vec2f;
 
     @vertex
-    fn vertexMain(@location(0) pos: vec2f,
-                  @builtin(instance_index) instance: u32) -> 
-      @builtin(position) vec4f {
+    fn vertexMain(input: VertexInput) -> VertexOutput {
       let i = f32(instance);
       let cell = vec2f(i % grid.x, floor(i / grid.x));
       let cellOffset = cell / grid * 2;
       let gridPos = (pos + 1) / grid - 1 + cellOffset;
 
-      const VertexOutput: VertexOutput = {
-        pos: vec4f(gridPos, 0, 1),
-        cell: cell
-      };
-      return VertexOutput;
+      var output: VertexOutput;
+      output.pos = vec4f(gridPos, 0, 1);
+      output.cell = cell;
+      return output;
     }
 
     @fragment
-    fn fragmentMain(@location(0) cell: vec2f) -> @location(0) vec4f {
-      return vec4f(cell, 0, 1);
+    fn fragmentMain(input: FragInput) -> @location(0) vec4f {
+      return vec4f(input.cell/grid, 0, 1);
     }
   `
 });
