6bcaf42cc51f9bc9f64c34215bb146d109c6eb50
sreiswig <sam.reiswig@gmail.com>
2024-10-07 07:52:13 -0500

    fix syntax

--- hello_web_gpu.js
+++ hello_web_gpu.js
@@ -50,12 +50,12 @@ const vertexBufferLayout = {
   }],
 };
 
-const VertexInput {
+const VertexInput = {
   @location(0) pos: vec2f,
   @builtin(instance_index) instance: u32,
 };
 
-const VertexOutput {
+const VertexOutput = {
   @builtin(position) pos: vec4f,
   @location(0) cell: vec2f,
 };
